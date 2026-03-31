9740455140122a1661ab7ca7c5558caa9273137b
Millenimus <adress@emailexample.com>
2026-03-31 19:19:10 +0300

    function substruct has been added

--- main.py
+++ main.py
@@ -2,5 +2,8 @@
 print ("Welcom to the calculator!")
 def add(a, b)
 	return a+b
+def substruct(a, b)
+	return a-b
 if _name_=="_main_":
 	print (f"2+2 = {add(2,2)}")
+	print (f"10-3 ={substruct(10,3)}") #<--new string
